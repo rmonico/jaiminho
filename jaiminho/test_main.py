1f57275df96a7bf0625b24e7c1c6c032537bb460
Rafael Monico <rmonico@moningrado>
2022-11-22 22:24:47 -0300

    Removed tests for old code

--- jaiminho/test_main.py
+++ jaiminho/test_main.py
@@ -13,50 +13,6 @@ class JaiminhoTests(TestCase):
 
         self.assertEqual(request_file, '/home/folder/ab/cd/de.yaml')
 
-    def test_namespace_default(self):
-        default = main._namespace('ab/cd/de')
-
-        self.assertEqual(default, 'ab/cd')
-
-    def test_make_extensions_should_append_property(self):
-        arguments = [{'property': 'value'}, {'another thing': False}]
-
-        definitive = main._make_extensions(arguments)
-
-        self.assertDictEqual(definitive, {
-            'property': 'value',
-            'another thing': False
-        })
-
-    def test_make_extensions_should_overwrite_property(self):
-        arguments = [{'property': 'value'}, {'property': 'another value'}]
-
-        definitive = main._make_extensions(arguments)
-
-        self.assertDictEqual(definitive, {'property': 'another value'})
-
-    def test_make_extensions_should_append_property_in_subkeys(self):
-        arguments = [{
-            'key': {
-                'property': 'value'
-            }
-        }, {
-            'key': {
-                'another thing': False
-            }
-        }]
-
-        definitive = main._make_extensions(arguments)
-
-        self.assertDictEqual(
-            definitive, {'key': {
-                'property': 'value',
-                'another thing': False
-            }})
-
-    def test_make_extensions_should_extend_external_file(self):
-        pass
-
 
 if __name__ == '__main__':
     unittest_main()
