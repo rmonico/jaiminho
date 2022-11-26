e4d5c75ab57c7354dc4517adab19040b00a7f43f
Rafael Monico <rmonico@moningrado>
2022-11-26 01:25:23 -0300

    Tests for existing code

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,11 +17,47 @@ class JaiminhoTests(TestCase):
     def test_GIVEN_a_dict_with_only_non_dict_values_THEN_must_return_them_unchanged(self):
         d = { 'a': 99 }
 
-        retornado = main._convert_dicts_to_namespace(d)
+        returned = main._convert_dicts_to_namespace(d)
 
-        self.assertEqual(type(retornado), dict)
-        self.assertIn('a', retornado)
-        self.assertEqual(retornado['a'], 99)
+        self.assertEqual(type(returned), dict)
+        self.assertIn('a', returned)
+        self.assertEqual(returned['a'], 99)
+
+
+    def test_GIVEN_a_dict_with_dict_values_THEN_must_return_them_as_namespaces(self):
+        d = { 'a': { 'b': 'value' } }
+
+        returned = main._convert_dicts_to_namespace(d)
+
+        self.assertEqual(type(returned), dict)
+        self.assertIn('a', returned)
+        self.assertIsInstance(returned['a'], Namespace)
+        self.assertTrue(hasattr(returned['a'], 'b'))
+        self.assertEqual(returned['a'].b, 'value')
+
+
+    def test_GIVEN_a_dict_with_variables_THEN_must_interpolate_its_keys_on_a_format_string(self):
+        d = { 'var': 'value', 'another_var': 'another value' }
+
+        returned = main._format_all_strs(d, 'value of var: {var}; value of another_var: {another_var}')
+
+        self.assertEqual(returned, 'value of var: value; value of another_var: another value')
+
+
+    def test_GIVEN_a_dict_with_namespace_variables_THEN_must_interpolate_its_keys_on_a_format_string(self):
+        d = { 'var': Namespace(prop='value'), 'another': Namespace(other='another value') }
+
+        returned = main._format_all_strs(d, 'value of var: {var.prop}; value of another: {another.other}')
+
+        self.assertEqual(returned, 'value of var: value; value of another: another value')
+
+
+    def test_GIVEN_a_dict_with_dicts_THEM_must_interpolate_strings_inside_them(self):
+        d = { 'a': 'value', 'b': 'b value' }
+
+        returned = main._format_all_strs_on_dict(d, { 1: 'a: {a}', 2: 'b: {b}' })
+
+        self.assertDictEqual(returned, { 1: 'a: value', 2: 'b: b value' })
 
 
 if __name__ == '__main__':
