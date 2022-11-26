44993f16a9f2a8b8ad37b4d922dcb40a286760dc
Rafael Monico <rmonico@moningrado>
2022-11-25 23:58:02 -0300

    Main module test moved and new method created

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,5 +14,15 @@ class JaiminhoTests(TestCase):
         self.assertEqual(request_file, '/home/folder/ab/cd/de.yaml')
 
 
+    def test_GIVEN_a_dict_with_only_non_dict_values_THEN_must_return_them_unchanged(self):
+        d = { 'a': 99 }
+
+        retornado = main._convert_dicts_to_namespace(d)
+
+        self.assertEqual(type(retornado), dict)
+        self.assertIn('a', retornado)
+        self.assertEqual(retornado['a'], 99)
+
+
 if __name__ == '__main__':
     unittest_main()
