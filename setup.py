92f96962238c66d301de809ff547f9b77c65ebc9
Rafael Monico <rmonico@moningrado>
2023-05-31 16:58:31 -0300

    build: version updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jaiminho',
-      version='0.0.1',
+      version='0.0.2',
       description='A CLI rest client',
       url='https://github.com/rmonico/jaiminho',
       author='Rafael Monico',
