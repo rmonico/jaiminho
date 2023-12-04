101f67cb693e7074b4c6b9ef94d8d38bfea6c285
Rafael Monico <rmonico@notevia>
2023-12-04 16:52:39 -0300

    fix: ignoring __environment__ files on list

--- jaiminho/commands/list.py
+++ jaiminho/commands/list.py
@@ -9,6 +9,9 @@ def _print(folder_path, base_path_length, indent_level=0):
         for entry in it:
             if entry.is_dir():
                 _print(os.path.join(folder_path, entry.name), base_path_length, indent_level+1)
-            elif entry.is_file() and entry.name.endswith(('.yaml', '.yml')):
+            elif _is_request_file(entry):
                 print(entry.path[base_path_length:-5])
+
+def _is_request_file(entry: str) -> bool:
+    return entry.is_file() and '__environment__' not in entry.name and entry.name.endswith(('.yaml', '.yml'))
 
