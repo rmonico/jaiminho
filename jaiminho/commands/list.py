b30249fbebf8ccf0fcf9d60b185e5f73f8086d1d
Rafael Monico <rmonico@notevia>
2024-02-01 20:36:40 -0300

    feat(9bf13bd1): showing method before request name

--- jaiminho/commands/list.py
+++ jaiminho/commands/list.py
@@ -10,8 +10,19 @@ def _print(folder_path, base_path_length, indent_level=0):
             if entry.is_dir():
                 _print(os.path.join(folder_path, entry.name), base_path_length, indent_level+1)
             elif _is_request_file(entry):
-                print(entry.path[base_path_length:-5])
+                _print_request(base_path_length, entry.path)
 
 def _is_request_file(entry: str) -> bool:
     return entry.is_file() and '__environment__' not in entry.name and entry.name.endswith(('.yaml', '.yml'))
 
+def _print_request(base_path_length, request_file_name):
+    with open(request_file_name) as request_file:
+        import yaml
+
+        request = yaml.safe_load(request_file)
+
+    method = request['request']['method']
+
+    request_name = request_file_name[base_path_length:-5]
+
+    print(f'{method.upper().ljust(7)} {request_name}')
